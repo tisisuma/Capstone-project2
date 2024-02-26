ae024efc66a42a8e24bce96d1909ac8a15b356fd
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:28:35 +0530

    Delete TC_PIM_01 directory

--- TC_PIM_01/Data/data.py
+++
@@ -1,5 +0,0 @@
-class Web_Data:
-   url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-   username = "umaraja"
-   reseturl="https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode"
-   Confirmmessage="A reset password link has been sent to you via email."
