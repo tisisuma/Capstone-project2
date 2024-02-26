d7dd6f8c45a186d4201882a90c5577f1410e8f48
tisisuma <152303037+tisisuma@users.noreply.github.com>
2024-02-26 10:28:48 +0530

    Delete TC_PIM_02 directory

--- TC_PIM_02/Data/data.py
+++
@@ -1,6 +0,0 @@
-class Web_Data:
-   url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-   username = "Admin"
-   password = "admin123"
-   dashboardurl ="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
-   adminurl="https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers"
